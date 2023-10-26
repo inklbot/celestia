1e950850b1b5acc24f0f8d4c2e022b2440528c2e
inklbot <31788091+inklbot@users.noreply.github.com>
2023-10-27 08:51:21 +0900

    Update celestia.sh

--- celestia.sh
+++ celestia.sh
@@ -7,7 +7,7 @@ update_packages() {
 }
 
 go_install() {
-    ver="1.20.2" 
+    ver="1.21.1" 
     cd $HOME 
     wget "https://golang.org/dl/go$ver.linux-amd64.tar.gz" 
     sudo rm -rf /usr/local/go 
